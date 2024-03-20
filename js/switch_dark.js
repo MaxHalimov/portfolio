e07eeb511057fef09adf466cdd12159afbbfff5c
Ayanokoji <h_mahmadali@mail.ru>
2024-03-20 15:30:36 +0500

    some features

--- js/switch_dark.js
+++ js/switch_dark.js
@@ -1,76 +1,31 @@
-// const themeButton = document.getElementById("themeButton");
-// const theme_logo = document.getElementById("logo");
-// const btnThem = document.getElementById("btnTheme");
-// const facebook = document.getElementById("iconFacebook");
-// const inst = document.getElementById("iconInst");
-// const twit = document.getElementById("iconTwit");
-// const git = document.getElementById("iconGit");
-// const linkedIn = document.getElementById("iconIn");
-// const telegram = document.getElementById("iconTg");
-// const underline = document.getElementById("line");
-
-// // let changeText = document.getElementById("textChange");
-
-// // function changeTextAutomatically() {
-// //   let changeText = document.getElementById("textChange");
-// //   let currentText = changeText.innerText;
-
-// //   if (currentText === "Привет!") {
-// //     textBlock.innerText = "Hallo!";
-// //   } else if (currentText === "Hallo!") {
-// //     textBlock.innerText = "Салом!";
-// //   } else if (currentText === "Салом!") {
-// //     textBlock.innerText = "Привет!";
-// //   }
-// // }
-// // Вызываем функцию changeTextAutomatically каждые 3 секунды (3000 миллисекунд)
-// // setInterval(changeTextAutomatically, 3000);
-
-// themeButton.addEventListener("click", () => {
-//   document.body.classList.toggle("dark");
-
-//   if (document.body.classList.contains("dark")) {
-//     theme_logo.setAttribute("src", "./assets/logo_white.svg");
-//   } else theme_logo.setAttribute("src", "./assets/logo.svg");
-
-//   if (document.body.classList.contains("dark")) {
-//     btnThem.setAttribute("src", "./assets/sun.svg");
-//   } else btnThem.setAttribute("src", "./assets/moon.svg");
-
-//   if (document.body.classList.contains("dark")) {
-//     facebook.setAttribute("src", "./assets/facebookDark.svg");
-//   } else facebook.setAttribute("src", "./assets/facebook.svg");
-
-//   if (document.body.classList.contains("dark")) {
-//     inst.setAttribute("src", "./assets/instDark.svg");
-//   } else inst.setAttribute("src", "./assets/instagram.svg");
-
-//   if (document.body.classList.contains("dark")) {
-//     twit.setAttribute("src", "./assets/twitDark.svg");
-//   } else twit.setAttribute("src", "./assets/twitter.svg");
-
-//   if (document.body.classList.contains("dark")) {
-//     git.setAttribute("src", "./assets/gitDark.svg");
-//   } else git.setAttribute("src", "./assets/github.svg");
-
-//   if (document.body.classList.contains("dark")) {
-//     linkedIn.setAttribute("src", "./assets/inDark.svg");
-//   } else linkedIn.setAttribute("src", "./assets/linkedin.svg");
-
-//   if (document.body.classList.contains("dark")) {
-//     telegram.setAttribute("src", "./assets/tgDark.svg");
-//   } else telegram.setAttribute("src", "./assets/telegram.svg");
-
-//   if (document.body.classList.contains("dark")) {
-//     underline.setAttribute("src", "./assets/lineForDark.svg");
-//   } else underline.setAttribute("src", "./assets/line.svg");
-
-//   if (document.body.classList.contains('dark')) {
-//     vec
-//   }
-// });
 const themeButton = document.getElementById("themeButton");
 const themeButton2 = document.getElementById("themeButtonMob");
+
+function changeTitle() {
+  const textArr = ["Hallo!", "Hello!", "Привет!", "Салом!"];
+  let i = 0;
+
+  function updateText() {
+    const textElement = document.getElementById("textChange");
+    textElement.classList.add("fadeOut");
+
+    setTimeout(function () {
+      textElement.textContent = textArr[i];
+      textElement.classList.remove("fadeOut");
+      i = (i + 1) % textArr.length;
+    }, 750); // Длительность анимации / 2 (в миллисекундах)
+
+    setTimeout(updateText, 3000); // Вызываем функцию себя через каждые 3 секунды
+  }
+
+  // Начинаем анимацию сразу
+  updateText();
+}
+
+
+window.onload = function () {
+  changeTitle();
+};
 
 const elements = {
   logo: ["./assets/logo.svg", "./assets/logo_white.svg"],
